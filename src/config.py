58f321fba36d15a4274aa2807410280b0467aaf3
ilyabudkovoy <ilyabudkovoy@yandex.ru>
2025-06-26 17:21:43 +0300

    feat: Add Vault client for using secret passwords
    - Create HvacClient class;
    - Add pre-commit file;
    - Create first version of AuthX config;

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 from authx import AuthXConfig
 from dotenv import load_dotenv
-
+from src.vault import hvac_client
 
 load_dotenv()
 
 config = AuthXConfig()
-config.JWT_SECRET_KEY = ""
+config.JWT_SECRET_KEY = hvac_client.read_item(path="jwt", mount_point="kv/data", key="JWT_SECRET_KEY")
