52a9004683f32ba6b70d63690b2f412bc36ab304
ilyabudkovoy <ilyabudkovoy@yandex.ru>
2025-07-11 17:06:16 +0300

    fix: Edit file structure

--- src/config/auth.py
+++ src/config/auth.py
@@ -5,8 +5,10 @@ from src.vault import hvac_client
 load_dotenv()
 
 config = AuthXConfig()
-config.JWT_SECRET_KEY = hvac_client.read_item(path="jwt", mount_point="kv/data", key="JWT_SECRET_KEY")
+config.JWT_SECRET_KEY = hvac_client.read_item(
+    path="jwt", mount_point="kv/data", key="JWT_SECRET_KEY"
+)
 config.JWT_ACCESS_COOKIE_NAME = "access_token"
-config.JWT_TOKEN_LOCATION = ("cookies", )
+config.JWT_TOKEN_LOCATION = ("cookies",)
 
 security = AuthX(config=config)
